33e81f150cb6a1c79474685567d4a9b77b634012
josohyeon <josohyun0@naver.com>
2021-05-24 10:47:22 +0900

    Revert "Revert "monitor""
    This reverts commit 7017bdf29b5acb3650ac400412186d7956f4ce54.

---
+++ monitor/02_farm_mnt/0206.php
@@ -0,0 +1,26 @@
+<?
+include_once("../inc/top.php");
+?>
+<!--결함 및 A/S 관리-->
+<div class="row fullSc">
+	<article class="col-xl-12">
+		<div class="jarviswidget jarviswidget-color-darken" data-widget-editbutton="false" data-widget-colorbutton="false" data-widget-deletebutton="false">
+			<header>
+				<div class="widget-header">	
+					<h2><i class="fa fa-table"></i>&nbsp;&nbsp;&nbsp;결함 및 A/S 관리</h2>	
+				</div>
+			</header>
+				
+			<div class="widget-body">
+
+				
+				
+			</div>
+					
+		</div>
+	</article>
+</div>
+
+<?
+include_once("../inc/bottom.php");
+?>
---
+++ monitor/04_device_mgr/0402.php
@@ -0,0 +1,26 @@
+<?
+include_once("../inc/top.php");
+?>
+<!--IP 카메라 관리-->
+<div class="row fullSc">
+	<article class="col-xl-12">
+		<div class="jarviswidget jarviswidget-color-darken" data-widget-editbutton="false" data-widget-colorbutton="false" data-widget-deletebutton="false">
+			<header>
+				<div class="widget-header">	
+					<h2><i class="fa fa-table"></i>&nbsp;&nbsp;&nbsp;IP 카메라 관리</h2>	
+				</div>
+			</header>
+
+			<div class="widget-body">
+
+				
+				
+			</div>
+					
+		</div>
+	</article>
+</div>
+
+<?
+include_once("../inc/bottom.php");
+?>
