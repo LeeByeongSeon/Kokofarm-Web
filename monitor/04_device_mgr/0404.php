0ab790459abc8e0ef0ac96a4328c35fbca8041b3
josohyeon <josohyun0@naver.com>
2021-05-25 15:35:57 +0900

    0404.php 수정

--- monitor/04_device_mgr/0404.php
+++ monitor/04_device_mgr/0404.php
@@ -1,14 +1,14 @@
 <?
 include_once("../inc/top.php");
 ?>
-<!--급이/급수/외기 관리-->
+<!--급이 / 급수 관리-->
 <article class="col-xl-10 float-right">
 	<div class="row">
 		<div class="col-xl-12">
 			<div class="jarviswidget jarviswidget-color-teal no-padding" data-widget-editbutton="false" data-widget-colorbutton="false" data-widget-deletebutton="false" data-widget-fullscreenbutton="false" data-widget-togglebutton="false">
 				<header>
 					<div class="widget-header">	
-						<h2><i class="fa fa-tablet"></i>&nbsp;&nbsp;&nbsp;급이/급수/외기 관리</h2>	
+						<h2><i class="fa fa-tablet"></i>&nbsp;&nbsp;&nbsp;급이 / 급수 관리</h2>	
 					</div>
 				</header>
 					
@@ -16,51 +16,17 @@ include_once("../inc/top.php");
 
 					<table class="table table-bordered table-hover" style="text-align: center;">
 						<thead>
-							<th></th>
-							<th>농장ID</th>
-							<th>동ID</th>
-							<th>IP주소</th>
-							<th>급이기</th>
-							<th>급수기</th>
-							<th>외기환경</th>
+							<tr>
+								<th></th>
+								<th>농장ID</th>
+								<th>동ID</th>
+								<th>사료 빈 총 용량</th>
+								<th>유량 센서 최대 펄스 값</th>
+								<th>외기환경 존재 유/무</th>
+							</tr>
 						</thead>
 						<tbody>
-							<tr>
-								<td>1</td>
-								<td>KF0020</td>
-								<td>01</td>
-								<td>59.2.28.197</td>
-								<td>Y</td>
-								<td>Y</td>
-								<td>Y</td>
-							</tr>
-							<tr>
-								<td>2</td>
-								<td>KF0020</td>
-								<td>01</td>
-								<td>59.2.28.197</td>
-								<td>Y</td>
-								<td>Y</td>
-								<td>N</td>
-							</tr>
-							<tr>
-								<td>3</td>
-								<td>KF0020</td>
-								<td>01</td>
-								<td>59.2.28.197</td>
-								<td>Y</td>
-								<td>Y</td>
-								<td>N</td>
-							</tr>
-							<tr>
-								<td>4</td>
-								<td>KF0020</td>
-								<td>01</td>
-								<td>59.2.28.197</td>
-								<td>Y</td>
-								<td>Y</td>
-								<td>N</td>
-							</tr>
+							
 						</tbody>
 					</table>
 					
