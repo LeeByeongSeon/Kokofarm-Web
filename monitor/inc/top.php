75a6cfd42f27391310246b49485a23f7d72bdac8
이병선 <bslee@demotion.co.kr>
2021-05-26 13:58:09 +0900

    tt

--- monitor/inc/top.php
+++ monitor/inc/top.php
@@ -99,18 +99,19 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
   	<!--<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,300,400,500,700">-->
   	<link rel="stylesheet" href="../../common/library/fonts/font.css"> <!--Google fonts-->
+
+	<!-- FAVICONS -->
+	<link rel="shortcut icon" href="../images/icon.png" type="image/x-icon">
+	<link rel="icon" href="../images/icon.png" type="image/x-icon">
 	
 	<script src="../../common/library/jquery/jquery.min.js"></script>						<!-- jQuery -->
 	<script src="../../common/library/jquery/jquery-ui-1.10.3.min.js"></script>				<!-- jQuery UI-->
 	<script src="../../common/library/bootstrap/bootstrap.min.js"></script>					<!-- BOOTSTRAP JS -->
 	<script src="../../common/library/smartwidgets/jarvis.widget.min.js"></script>			<!-- JARVIS WIDGETS -->
 	<script src="../../common/library/plugin/sparkline/jquery.sparkline.min.js"></script>	<!-- SPARKLINES -->
-	
-	<!-- FAVICONS -->
-	<link rel="shortcut icon" href="../images/icon.png" type="image/x-icon">
-	<link rel="icon" href="../images/icon.png" type="image/x-icon">
 
 	<!--Bootsteap Table-->
 	<link rel="stylesheet" href="../../common/library/bootstrap_table/bootstrap-table.css"/>
