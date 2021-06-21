d9ae71f055e885759ab9ef73b5056b5c47f4e636
josohyeon <josohyun0@naver.com>
2021-06-21 11:44:21 +0900

    0103 가운데 정렬

--- monitor/01_device_mnt/camera_popup.php
+++ monitor/01_device_mnt/camera_popup.php
@@ -8,14 +8,17 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes">
+	    <link rel="stylesheet" type="text/css" media="screen" href="../../common/library/bootstrap/bootstrap.min.css">
     </head>
 
     <body>
-        <button type="button" onClick="zoom('+')">확대</button>
-        <button type="button" onClick="zoom('-')">축소</button>
         <div id="camera_popup">
-            <img id="camera_img" src="../images/noimage.jpg" style="opacity: 1.0; filter: alpha(opacity=100);">
-        </div><!--modal -->
+            <div style="width:120px; padding:5px" class="center-block">
+                <button class="btn btn-primary" type="button" onClick="zoom('+')">확대</button>
+                <button class="btn btn-default" type="button" onClick="zoom('-')">축소</button>
+            </div>
+        </div>
+        <img class="img-responsive center-block" id="camera_img" src="../images/noimage.jpg" style="opacity: 1.0; filter: alpha(opacity=100); margin:auto">
     </body>
 </html>
 
