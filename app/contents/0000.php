ffa03dd0f234d7a5c6856b05f753ec9d3c171e40
DESKTOP-FVKCJ6E\bslee <bslee@demotion.co.kr>
2022-04-12 18:21:04 +0900

    사료빈 없는 경우 온도 추가

--- app/contents/0000.php
+++ app/contents/0000.php
@@ -12,18 +12,30 @@ foreach($init_data as $val){
 	$dong_list_html .= "			<div class='widget-body no-padding form-inline move_dong' style='border-radius: 10px; border : 4px solid #eee; border-top: 0;' onClick='move_dong(\"".$val["fdDongid"]."\")'>";
 	$dong_list_html .= "				<div class='col-xs-2 text-center no-padding'><p><span class='fong-md font-weight-bold' style='font-size:20px'>".$val["fdDongid"]."동</span></p><span id=''>".$val["interm"]."일령</span></div>";
 	$dong_list_html .= "				<div class='col-xs-3 text-center no-padding'><p><span class='fong-md font-weight-bold' style='font-size:12px' >평균중량</span></p><span class='font-lg text-danger font-weight-bold'>".sprintf('%0.1f', $val["beAvgWeight"])."g</span></div>";
-	$dong_list_html .= "				<div class='col-xs-1 text-center no-padding'><i class='fa fa-circle text-success'></i></div>";
-	//$dong_list_html .= "<div class='col-xs-3 text-center no-padding'><img src='../images/feed-00.png' style='width:40px'></div>";
-	$dong_list_html .= "				<div class='col-xs-3 text-center no-padding'><img src='";
-	if($val["sfFeed"]>0){
-		$per = round($val["sfFeed"] / $val["sfFeedMax"] * 100);
+	//$dong_list_html .= "				<div class='col-xs-1 text-center no-padding'><i class='fa fa-circle text-success'></i></div>";
+
+	// 사료빈 데이터가 있으면
+	if($val["sfDongid"] == ""){
+		$dong_list_html .= "			<div class='col-xs-3 text-center no-padding'>
+											<p><span class='fong-md font-weight-bold' style='font-size:12px' >평균온도</span></p>
+											<span class='font-lg font-weight-bold'>".sprintf('%0.1f', $val["beAvgTemp"])."℃</span>
+										</div>";
 	}
-	if($per <= 10)						$dong_list_html .= "../images/feed-00.png' style='width:40px";
-	else if($per > 10 && $per <= 35)	$dong_list_html .= "../images/feed-01.png' style='width:40px";
-	else if($per > 35 && $per <= 65)	$dong_list_html .= "../images/feed-02.png' style='width:40px";
-	else if($per > 65 && $per <= 90)	$dong_list_html .= "../images/feed-03.png' style='width:40px";
-	else if($per > 90)					$dong_list_html .= "../images/feed-04.png' style='width:40px";
-	$dong_list_html .= "' style='width:40px'> <p><span class='fong-md font-weight-bold' style='font-size:12px'>" . $val["sfFeed"] . "(Kg)</span></p> </div>";
+	else{
+		// 사료빈 이미지 넣기
+		$dong_list_html .= "				<div class='col-xs-3 text-center no-padding'><img src='";
+		if($val["sfFeed"]>0){
+			$per = round($val["sfFeed"] / $val["sfFeedMax"] * 100);
+		}
+		if($per <= 10)						$dong_list_html .= "../images/feed-00.png'";
+		else if($per > 10 && $per <= 35)	$dong_list_html .= "../images/feed-01.png'";
+		else if($per > 35 && $per <= 65)	$dong_list_html .= "../images/feed-02.png'";
+		else if($per > 65 && $per <= 90)	$dong_list_html .= "../images/feed-03.png'";
+		else if($per > 90)					$dong_list_html .= "../images/feed-04.png'";
+		$dong_list_html .= "' style='width:40px'> <p><span class='fong-md font-weight-bold' style='font-size:12px'>" . $val["sfFeed"] . "(Kg)</span></p> </div>";
+	}
+
+	$dong_list_html .= "				<div class='col-xs-1 text-center no-padding'><i class='fa fa-circle text-success'></i></div>";
 
 	$live_count = $val["cmInsu"] - $val["cmDeathCount"] - $val["cmCullCount"] - $val["cmThinoutCount"];
 
